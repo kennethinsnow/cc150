02e2b947bfc8cf8fdc7cc66f1b0415da4820fec6
Ke Liao <liaoke@gmail.com>
2015-04-20 16:09:19 -0700

    change to question 19_5

--- Chapter 19/Question19_5/Question.java
+++ Chapter 19/Question19_5/Question.java
@@ -15,8 +15,13 @@ public class Question {
 		Result res = new Result();
 		int solution_mask = 0;
 		for (int i = 0; i < 4; ++i) {
+			System.out.println("solution mask:" + solution_mask);
+			System.out.println("charat: " + Integer.toBinaryString((solution.charAt(i) - 'A' + 1)));
+			System.out.println("charat shifted: " + Integer.toBinaryString(( 1 << (solution.charAt(i) - 'A' + 1))));
 			solution_mask |= 1 << (1 + solution.charAt(i) - 'A');
+			
 		}
+		System.out.println(solution_mask);
 		for (int i = 0; i < 4; ++i) {
 			if (guess.charAt(i) == solution.charAt(i)) {
 				++res.hits;
@@ -28,8 +33,13 @@ public class Question {
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Result res = estimate("YRGB", "RGGB");
+		Result res = estimate("YYGB", "RGGB");
 		System.out.println(res.toString());
+		int num = 30;
+		System.out.println("left shift experiment: " + (1 << num));
+		System.out.println("left shift experiment: " + Integer.toBinaryString(1 << num));
+		System.out.println("left shift experiment: " + (-1 << num));
+		System.out.println("left shift experiment: " + Integer.toBinaryString(-1 << num));
 	}
 
 }
